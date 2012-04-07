3deb6ab55c6d19a92bc2d028e0f8782ad6a3ddb1
Chandler Abraham <chandler@pagerduty.com>
2012-04-06 18:51:55 -0700

    jquery-ui, site updater, tons of fiddling with message_locations ui

--- app/assets/javascripts/charts/message_location.js
+++ app/assets/javascripts/charts/message_location.js
@@ -3,9 +3,21 @@
     // a = $('#message_location').attr('data')
     // obj = JSON.parse(a);
     // 47cb42916aae4beb8b9f3bf68ac4e3e8
+
+
 $(function(){
   if ($('#message_location').length > 0){
-   
+
+    $( "#slider" ).slider({
+        value: 37,
+        min: 1,
+        max: 700,
+        slide: function( event, ui ) {
+            $( "#amount" ).val( "$" + ui.value );
+        }
+    });
+    $( "#amount" ).val( "$" + $( "#slider" ).slider( "value" ) );
+
     // a = $('#message_location').attr('data')
     // obj = JSON.parse(a);
 
@@ -14,88 +26,62 @@ $(function(){
 
     var map = po.map()
         .container(document.getElementById("graphbox").appendChild(svg))
-        .center({lat: 37.787, lon: -122.228})
-        .zoom(14)
-        .zoomRange([12, 16])
+        .center({lat: 37.808522, lon: -122.364581})
+        .zoom(12)
+        .zoomRange([12, 13])
         .add(po.interact());
 
     map.add(po.image()
         .url(po.url("http://{S}tile.cloudmade.com"
         + "/47cb42916aae4beb8b9f3bf68ac4e3e8" // http://cloudmade.com/register
-        + "/20760/256/{Z}/{X}/{Y}.png")
+        + "/998/256/{Z}/{X}/{Y}.png")
         .hosts(["a.", "b.", "c.", ""])));
 
     map.add(po.geoJson()
-        .url("/assets/message_location.json")
+        .url("/assets/location.json")
         .on("load", load)
         .clip(false)
         .zoom(14));
 
+    // function load(e) {
+    //   var cluster = e.tile.cluster || (e.tile.cluster = kmeans()
+    //       .iterations(2)
+    //       .size(1000));
 
-    /* Create a shadow filter. */
-    map.add("svg:filter")
-        .attr("id", "shadow")
-        .attr("width", "140%")
-        .attr("height", "140%")
-      .add("svg:feGaussianBlur")
-        .attr("in", "SourceAlpha")
-        .attr("stdDeviation", 3);
+    //   for (var i = 0; i < e.features.length; i++) {
+    //     cluster.add(e.features[i].data.geometry.coordinates);
+    //   }
 
-    /* Create radial gradient r1. */
-    svg.add("svg:radialGradient")
-        .attr("id", "r1")
-        .attr("fx", 0.5)
-        .attr("fy", 0.9)
-      .add("svg:stop")
-        .attr("offset", "0%")
-        .attr("stop-color", "#00bf17")
-        .parent()
-      .add("svg:stop")
-        .attr("offset", "100%")
-        .attr("stop-color", "#0f2f13");
+    //   var tile = e.tile, g = tile.element;
+    //   while (g.lastChild) g.removeChild(g.lastChild);
 
-    /* Create radial gradient r2. */
-    svg.add("svg:radialGradient")
-        .attr("id", "r2")
-        .attr("fx", 0.5)
-        .attr("fy", 0.1)
-      .add("svg:stop")
-        .attr("offset", "0%")
-        .attr("stop-color", "#cccccc")
-        .parent()
-      .add("svg:stop")
-        .attr("offset", "100%")
-        .attr("stop-color", "#cccccc")
-        .attr("stop-opacity", 0);
+    //   var means = cluster.means();
+    //   means.sort(function(a, b) { return b.size - a.size; });
+    //   for (var i = 0; i < means.length; i++) {
+    //     var mean = means[i], point = g.appendChild(po.svg("circle"));
+    //     point.setAttribute("cx", mean.x);
+    //     point.setAttribute("cy", mean.y);
+    //     point.setAttribute("r", Math.pow(2, tile.zoom - 11) * Math.sqrt(mean.size));
+    //   }
+    // }
 
 
 
+     /** Post-process the GeoJSON points! */
     function load(e) {
-      var r = 20 * Math.pow(2, e.tile.zoom - 12);
+      var r =  20;
       for (var i = 0; i < e.features.length; i++) {
-        var c = e.features[i].element,
-            g = c.parent().add("svg:g", c);
+        var c = e.features[i]
 
-        g.attr("transform", "translate(" + c.attr("cx") + "," + c.attr("cy") + ")");
-
-        g.add("svg:circle")
-            .attr("r", r)
-            .attr("transform", "translate(" + r + ",0)skewX(-45)")
-            .attr("opacity", .5)
-            .attr("filter", "url(#shadow)");
-
-        g.add(c
-            .attr("fill", "url(#r1)")
-            .attr("r", r)
-            .attr("cx", null)
-            .attr("cy", null));
-
-        g.add("svg:circle")
-            .attr("transform", "scale(.95,1)")
-            .attr("fill", "url(#r2)")
-            .attr("r", r);
+        c.element.setAttribute("r", r);
+        if(c.data.properties.dayofweek == "weekend")
+            attr = "1"
+        else
+            attr = "2"
+        c.element.setAttribute("id", attr);
       }
     }
+
   } 
 });
 
